85ebebca1d66177b3829fe687660938f9ee9d48a
Chetan Timsina <chetantimsina01944@gmail.com>
2025-03-13 12:55:20 +0600

    Adding all file

---
+++ script.js
@@ -0,0 +1,11 @@
+// let About = document.querySelector("#About");
+// let about_drop = document.querySelector(".About-drop");
+// About.addEventListener("mouseover", () => {
+//   about_drop.classList.remove("hide");
+// });
+// About.addEventListener("mouseleave", () => {
+//   about_drop.classList.add("hide");
+// });
+// about_drop.addEventListener("mouseleave", () => {
+//   about_drop.classList.add("hide");
+// });
